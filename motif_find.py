674c135d87248f396350b7e0cbce3133ed7fbfe6
giladam <giladam@cs.huji.ac.il>
2019-12-16 10:17:23 +0200

    cleanup

--- motif_find.py
+++ motif_find.py
@@ -3,13 +3,40 @@ import numpy as np
 
 LETTER_TO_INDEX = {'A':0, 'C':1, 'G':2, 'T':3, '^':4, '$':5}
 
-def print_50(str1, str2):
-    i,j = 0,0
-    while i < len(str1)-1:
-        j = min(i+50, len(str1))
-        print(str1[i:j])
-        print(str2[i:j], '\n')
-        i = j
+def build_transition_matrix(num_states, motif_len, p, q):
+    transition_matrix = np.zeros((num_states, num_states))
+    transition_matrix[0, 1] = q
+    transition_matrix[0, -2] = 1-q
+    transition_matrix[1, 1] = 1-p
+    transition_matrix[1, 2] = p
+    transition_matrix[-2, -2] = 1-p
+    transition_matrix[-2, -1] = p
+    for i in range(motif_len):
+        transition_matrix[2+i, 3+i] = 1
+    print("### transition matrix ###\n", transition_matrix, "\n")
+    return transition_matrix
+
+def build_emission_matrix(initial_emissions, motif_len):
+    zero_row = np.zeros((1, 4))
+    zero_col = np.zeros((motif_len+4, 1))
+    bg_emissions = np.array([0.25, 0.25, 0.25, 0.25])
+    emission_matrix = np.vstack((zero_row, bg_emissions, initial_emissions, bg_emissions, zero_row))
+    emission_matrix = np.hstack((emission_matrix, zero_col, zero_col))
+    emission_matrix[0, -2] = 1
+    emission_matrix[-1, -1] = 1
+    print("### emission matrix ###\n", emission_matrix, "\n")
+    return emission_matrix
+
+def calculate_viterbi(num_states, seq, transition_matrix, emission_matrix):
+    viterbi_matrix = np.zeros((num_states, len(seq), 2))
+    viterbi_matrix[0, 0, 0] = 1
+    for j in range(1, len(seq)):
+        for i in range(num_states):
+            vec = viterbi_matrix[:,j-1,0] * transition_matrix[:,i]
+            viterbi_matrix[i, j, 0] = max(vec) * emission_matrix[i, LETTER_TO_INDEX[seq[j]]]
+            viterbi_matrix[i, j, 1] = np.argmax(vec)
+    print("### viterbi matrix ###\n", viterbi_matrix[:, :, 0], "\n")
+    return viterbi_matrix
 
 def calculate_forward(num_states, seq, transition_matrix, emission_matrix):
     forward_matrix = np.zeros((num_states, len(seq)))
@@ -31,6 +58,14 @@ def calculate_backward(num_states, seq, transition_matrix, emission_matrix):
     backward_matrix[0, 0] = 1
     result = np.sum(backward_matrix[:, 1])
     return backward_matrix, result
+
+def print_50(str1, str2):
+    i,j = 0,0
+    while i < len(str1)-1:
+        j = min(i+50, len(str1))
+        print(str1[i:j])
+        print(str2[i:j], '\n')
+        i = j
 
 def main():
     np.set_printoptions(precision=2)
@@ -54,39 +89,11 @@ def main():
     p = args.p
     q = args.q
 
-    # build transition matrix
-    transition_matrix = np.zeros((num_states, num_states))
-    transition_matrix[0, 1] = q
-    transition_matrix[0, -2] = 1-q
-    transition_matrix[1, 1] = 1-p
-    transition_matrix[1, 2] = p
-    transition_matrix[-2, -2] = 1-p
-    transition_matrix[-2, -1] = p
-    for i in range(motif_len):
-        transition_matrix[2+i, 3+i] = 1
-    print("### transition matrix ###\n", transition_matrix, "\n")
-
-    # build emission matrix
-    zero_row = np.zeros((1, 4))
-    zero_col = np.zeros((motif_len+4, 1))
-    bg_emissions = np.array([0.25, 0.25, 0.25, 0.25])
-    emission_matrix = np.vstack((zero_row, bg_emissions, initial_emissions, bg_emissions, zero_row))
-    emission_matrix = np.hstack((emission_matrix, zero_col, zero_col))
-    emission_matrix[0, -2] = 1
-    emission_matrix[-1, -1] = 1
-    print("### emission matrix ###\n", emission_matrix, "\n")
+    transition_matrix = build_transition_matrix(num_states, motif_len, p, q)
+    emission_matrix = build_emission_matrix(initial_emissions, motif_len)
 
     if args.alg == 'viterbi':
-
-        # calculate viterbi matrix
-        viterbi_matrix = np.zeros((num_states, seq_len, 2))
-        viterbi_matrix[0, 0, 0] = 1
-        for j in range(1, seq_len):
-            for i in range(num_states):
-                vec = viterbi_matrix[:,j-1,0] * transition_matrix[:,i]
-                viterbi_matrix[i, j, 0] = max(vec) * emission_matrix[i, LETTER_TO_INDEX[seq[j]]]
-                viterbi_matrix[i, j, 1] = np.argmax(vec)
-        print("### viterbi matrix ###\n", viterbi_matrix[:, :, 0], "\n")
+        viterbi_matrix = calculate_viterbi(num_states, seq, transition_matrix, emission_matrix)
 
         # traceback state sequence
         index = int(np.argmax(viterbi_matrix[:, -1, 0]))
@@ -98,6 +105,7 @@ def main():
         result = ''.join(['M' if int(trace[j]) > 1 and int(trace[j]) < (num_states-2) else 'B' for j in range(1, len(trace))])
         print("trace: ", trace)
         print("result: ", result)
+        print_50(seq[1:-1], result)
 
     elif args.alg == 'forward':
         forward_matrix, result = calculate_forward(num_states, seq, transition_matrix, emission_matrix)
@@ -113,9 +121,19 @@ def main():
         forward_matrix, result1 = calculate_forward(num_states, seq, transition_matrix, emission_matrix)
         backward_matrix, result2 = calculate_backward(num_states, seq, transition_matrix, emission_matrix)
         posterior_matrix = forward_matrix * backward_matrix
-        print(forward_matrix, "\n")
-        print(backward_matrix, "\n")
-        print(posterior_matrix, "\n")
+        print("### forward matrix ###\n", forward_matrix, "\n")
+        print("### backward matrix ###\n", backward_matrix, "\n")
+        print("### posterior matrix ###\n", posterior_matrix, "\n")
+
+        # traceback state sequence
+        trace = str(int(np.argmax(posterior_matrix[:, -2])))
+        for j in range(1, seq_len-1):
+            trace += str(int(np.argmax(posterior_matrix[:, -j-2])))
+        trace = trace[::-1]
+        result = ''.join(['M' if int(trace[j]) > 1 and int(trace[j]) < (num_states-2) else 'B' for j in range(1, len(trace))])
+        print("trace: ", trace)
+        print("result: ", result)
+        print_50(seq[1:-1], result)
 
 if __name__ == '__main__':
     main()
